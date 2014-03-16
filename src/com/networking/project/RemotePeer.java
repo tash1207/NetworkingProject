4a5e7441c1d66758b615ce28c90d51adae36d870
Marco Munizaga <marco.munizaga@ufl.edu>
2014-03-15 20:05:20 -0400

    first work on remote peer

---
+++ src/com/networking/project/RemotePeer.java
@@ -0,0 +1,45 @@
+package com.networking.project;
+
+public class RemotePeer {
+	
+	private byte[] bitfield;
+	
+	private int peerid;
+	private String address;
+	private int port;
+	
+	
+	
+	public RemotePeer(String address, int port){
+		
+	}
+	
+	public int getPeerid() {
+		return peerid;
+	}
+
+	/**
+	 * Compares two bitfields and sees if there is anything of interest.
+	 * Meaning if there is a piece that this remote peer has that the input
+	 * bitfield doesn't have.
+	 * @param message
+	 * @return
+	 */
+	public boolean hasInterestingPieces(byte[] otherBitfield){
+		return false;
+	}
+	
+	/**
+	 * Send a message to the remote peer. Will return true or false if the message was sent successfully.
+	 * @param message
+	 * @return boolean
+	 */
+	public boolean sendMessage(byte[] message){
+		return false;
+	}
+	
+	public boolean startConnection(){
+		return false;
+	}
+
+}
