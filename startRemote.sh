4c0e2908248b512993a847b9ae1782bd1e6a78e0
Marco Munizaga <marco.munizaga@ufl.edu>
2014-04-23 15:05:19 -0400

    Added a checkout to clean the git repo before pulling

--- startRemote.sh
+++ startRemote.sh
@@ -3,6 +3,7 @@
 # cat PeerInfo.cfg | while read a; do echo $a | awk '{print "ubuntu@" $2}' | xargs -I {} ssh {} -i ~/.ssh/schoolProject.pem 'sudo apt-get -y install openjdk-7-jdk' ; done
  cat PeerInfo.cfg | while read a; do echo $a | awk '{print "ubuntu@" $2}' | xargs -I {} ssh {} -i ~/.ssh/schoolProject.pem 'sudo killall java' ; done
  cat PeerInfo.cfg | while read a; do echo $a | awk '{print "ubuntu@" $2}' | xargs -I {} ssh {} -i ~/.ssh/schoolProject.pem 'sudo killall javac' ; done
+ cat PeerInfo.cfg | while read a; do echo $a | awk '{print "ssh ubuntu@"$2 " -i ~/.ssh/schoolProject.pem \"cd NetworkingProject; git checkout .\""}' | sh  ; done
  cat PeerInfo.cfg | while read a; do echo $a | awk '{print "ssh ubuntu@"$2 " -i ~/.ssh/schoolProject.pem \"cd NetworkingProject; git pull origin amazon\""}' | sh  ; done
  cat PeerInfo.cfg | while read a; do echo $a | awk '{print "ssh ubuntu@"$2 " -i ~/.ssh/schoolProject.pem \"cd NetworkingProject; make clean \""}' | sh  ; done
  cat PeerInfo.cfg | while read a; do echo $a | awk '{print "ssh ubuntu@"$2 " -i ~/.ssh/schoolProject.pem \"cd NetworkingProject; make bin \""}' | sh  ; done
